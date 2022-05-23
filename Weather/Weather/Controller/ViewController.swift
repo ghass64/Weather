8c2bba85725f97a7094fa1a5e50762a7d55350cf
Ghassan <ghass.64@live.com>
2022-05-23 17:45:28 +0400

    organize the files with MVVM design pattern

---
+++ Weather/Weather/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Ghassan Fathi Al Marei on 23/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
